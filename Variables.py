f9f849b4b3e3a65e9a1a00ecac4af2ce75acd68f
HAdenouden <hadenouden@hotmail.com>
2023-05-23 10:56:23 +0200

    Updated the main.py with the wingloading diagrams

--- Variables.py
+++ Variables.py
@@ -5,48 +5,42 @@ kts_mps = 0.5144
 kph_mps = 1 / 3.6
 ftpm_mps = 0.00508
 
-#weight
+#Weight
 g0 = 9.81 #gravitational acceleration [m/s^2]
-DesignWeight = 15 * g0 #design gross weight [N]
-
-#Take-off
-CL_TO = 0.97
-CD_TO = 0.0898
-mu_TO = 0.17
-s_TO = 600 * ft_m
+DesignWeight = 32 * g0 #design gross weight [N]
 
 #Cruise
-h_cruise = 400 * ft_m #cruise altitude [m]
-V_cruise = 58.3 * kts_mps #cruise speed [m/s]
-P_a_cruise = 1500 #power available during cruise [W]
+h_cruise = 120 #cruise altitude [m]
+V_cruise = 80*kph_mps #cruise speed [m/s]
+#P_a_cruise = 1500 #power available during cruise [W]
 
-#Climb performance
-ROC = 591 * ftpm_mps #Rate of Climb [m/s]
+#Climb performance (after VTOL part)
+ROC = 3 #Rate of Climb [m/s]
 V_climb = V_cruise * 0.8
 
 #Turn performance
 theta = 45 #bank angle [degree]
 
 #Service Ceiling
-h_service = 500 * ft_m #service ceiling [m]
+h_service = 3150 #service ceiling [m]
 V_max_service = 120 / kph_mps #maximum velocity at service ceiling [m/s]
 
 #Approach and Landing
 V_approach = 29.5 * kts_mps #approach speed [m/s]
 margin_stall = 1.1 #stall reserve factor [-]
 V_stall_approach = V_approach / margin_stall #stall speed in approach configuration [m/s]
-CL_max_approach = 1.3 #maximum lift coefficient in approach configuration [-]
-h_max_approach = 100 * ft_m #maximum altitude to initialise approach [m]
+CL_max_approach = 1.46 #maximum lift coefficient in approach configuration [-]
+h_max_approach = 30 #maximum altitude to initialise approach [m]
 
 #Basic Geometry and Initial Guesses
-AR = 9 #aspect ratio [-]
+AR = 7 #aspect ratio [-]
 CD_min = 0.0418 #minimum drag coefficient [-]
-Cd0 = 0.035 #zero-lift drag coefficient [-]
+Cd0 = 0.03 #zero-lift drag coefficient [-]
 WoS_max = 20 #maximum wing loading [kg/m^2]
 TtW_max = 0.8 #maximum thrust-to-weight ratio [-]
 P_max = 5 #maximum power available??? [W]
-eta_prop = 0.6 #propeller efficiency [-]
-TOP_prop = 100 #take-off parameter [-]
+eta_prop = 0.85 #propeller efficiency [-]
+#TOP_prop = 100 #take-off parameter [-]
 
 
 
--- main.py
+++ main.py
@@ -18,8 +18,8 @@ q_cruise = 0.5 * cruise.density * V_cruise**2
 #constants related to drag
 e0 = 1.78*(1-0.045*AR**0.68)-0.64 #relation from "small unmanned fixed-wing a/c design: a practical approach
 k = 1 / (np.pi * AR * e0) #drag polar coefficient
-print(e0)
-
+print("Approach speed: ", V_approach)
+print("Stall speed at approach:", V_stall_approach)
 
 #Ranges
 dt = 0.01
@@ -27,6 +27,7 @@ WoS_range_kgm2 = np.arange(dt, WoS_max, dt)
 WoS_range_Pa = WoS_range_kgm2 / Pa_kgm2
 TtW_range = np.arange(dt, TtW_max, dt)
 P_range_kW = np.arange(dt, P_max, dt)
+PoW_range_kW = P_range_kW / DesignWeight
 
 
 ##################### THIS IS ALL THRUST-TO-WEIGHT RELATIONS #####################
@@ -85,48 +86,69 @@ P_range_kW = np.arange(dt, P_max, dt)
 rho_correction_turn = (sealevel.density / cruise.density) ** 0.75
 n = 1/np.cos(theta/180*np.pi)
 TtW_turn = q_cruise * (CD_min / WoS_range_Pa + WoS_range_Pa * k * (n/q_cruise)**2)
-P_turn_kW = TtW_turn * DesignWeight * V_cruise / eta_prop * rho_correction_turn / 1000
+PoW_turn_kW = TtW_turn * V_cruise / eta_prop * rho_correction_turn / 1000
+P_turn_kW = PoW_turn_kW * DesignWeight
+#P_turn_kW = TtW_turn * DesignWeight * V_cruise / eta_prop * rho_correction_turn / 1000
 
 #Rate of Climb (ROC)
 rho_correction_ROC = (sealevel.density / sealevel.density) ** 0.75
 TtW_ROC = ROC / V_climb + CD_min * q_climb / WoS_range_Pa + k * WoS_range_Pa / q_climb
-P_ROC_kW = TtW_ROC * DesignWeight * V_climb / eta_prop * rho_correction_ROC / 1000
+PoW_ROC_kW = TtW_ROC * V_climb / eta_prop * rho_correction_ROC / 1000
+P_ROC_kW = PoW_ROC_kW * DesignWeight
+#P_ROC_kW = TtW_ROC * DesignWeight * V_climb / eta_prop * rho_correction_ROC / 1000
 
 #Cruise (cruise)
 rho_correction_cruise = (sealevel.density / cruise.density) ** 0.75
 TtW_cruise = q_cruise * CD_min / WoS_range_Pa + k / q_cruise * WoS_range_Pa
-P_cruise_kW = TtW_cruise * DesignWeight * V_cruise / eta_prop * rho_correction_cruise / 1000
+PoW_cruise_kW = TtW_cruise * V_cruise / eta_prop * rho_correction_cruise / 1000
+P_cruise_kW = PoW_cruise_kW * DesignWeight
+#P_cruise_kW = TtW_cruise * DesignWeight * V_cruise / eta_prop * rho_correction_cruise / 1000
 
 #Maximum Ceiling (MC)
 V_stall_MC = V_stall_approach * sealevel.density / ceiling.density
 WoS_MC_Pa = 0.5 * cruise.density * V_stall_MC**2 * np.sqrt(np.pi * AR * e0 * Cd0)
 WoS_MC_kgm2 = WoS_MC_Pa * Pa_kgm2
+print(V_stall_MC, AR, e0, Cd0)
 
 #Endurance (end)
 V_stall_end = V_stall_approach * sealevel.density / ceiling.density
 WoS_end_Pa = 0.5 * cruise.density * V_stall_end**2 * np.sqrt(3 * np.pi * AR * e0 * Cd0)
 WoS_end_kgm2 = WoS_end_Pa * Pa_kgm2
+print(V_stall_end)
 
 #Approach speed (app)
 WoS_app_Pa = q_sea * CL_max_approach
 WoS_app_kgm2 = WoS_app_Pa * Pa_kgm2
+print(CL_max_approach)
 
 
 
 
 #define the graphs
+plt.subplot(1, 2, 1)
+plt.axvspan(WoS_MC_kgm2[0], WoS_max+1, alpha=0.3, color="blue")
+plt.axvspan(WoS_end_kgm2[0], WoS_max+1, alpha=0.3, color="blue")
+plt.axvspan(WoS_app_kgm2[0], WoS_max+1, alpha=0.3, color="blue")
+plt.fill_between(WoS_range_kgm2, PoW_turn_kW, alpha=0.3, color="blue")
+plt.fill_between(WoS_range_kgm2, PoW_ROC_kW, alpha=0.3, color="blue")
+plt.fill_between(WoS_range_kgm2, PoW_cruise_kW, alpha=0.3, color="blue")
+plt.title("Power over Weight")
+plt.xlim(0, WoS_max)
+plt.xlabel("W/S [kg/m^2]")
+plt.ylim(0, P_max / DesignWeight)
+plt.ylabel("P/W [kW/N]")
+plt.subplot(1, 2, 2)
 plt.axvspan(WoS_MC_kgm2[0], WoS_max+1, alpha=0.3, color="blue")
 plt.axvspan(WoS_end_kgm2[0], WoS_max+1, alpha=0.3, color="blue")
 plt.axvspan(WoS_app_kgm2[0], WoS_max+1, alpha=0.3, color="blue")
 plt.fill_between(WoS_range_kgm2, P_turn_kW, alpha=0.3, color="blue")
 plt.fill_between(WoS_range_kgm2, P_ROC_kW, alpha=0.3, color="blue")
 plt.fill_between(WoS_range_kgm2, P_cruise_kW, alpha=0.3, color="blue")
-
-
-#setup the general plot
+plt.title("Power for Design Weight")
 plt.xlim(0, WoS_max)
 plt.xlabel("W/S [kg/m^2]")
 plt.ylim(0, P_max)
 plt.ylabel("P [kW]")
+
 plt.show()
 
